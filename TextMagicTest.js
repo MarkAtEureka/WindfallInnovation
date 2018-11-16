721a49aa97836a9047f86dfd24ba5d23fa0aea98
MarkAtEureka <44786078+MarkAtEureka@users.noreply.github.com>
2018-11-15 21:56:14 -0700

    Update TextMagicTest.js

--- TextMagicTest.js
+++ TextMagicTest.js
@@ -1,8 +1,12 @@
 /*  TextMagicTest
 *   Can we concatenate two fields on a 123FormBuilder form?
+*   https://raw.githubusercontent.com/MarkAtEureka/WindfallInnovation/master/TextMagicTest.js
+*
 */
-var mobileNumber = '';
-var smsDomain = '';
+$(document).ready(function() {
+    var mobileNumber = '';
+    var smsDomain = '';
 
-mobileNumber =  $('#form > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div > input[type="text"]').vlaue;
-alert(mobileNumber);
+    mobileNumber =  $('#form > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div > input[type="text"]').vlaue;
+    alert(mobileNumber);
+}
