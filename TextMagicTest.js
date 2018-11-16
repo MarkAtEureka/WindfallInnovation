d058e90983f15d1d5021401f7dffb09ba74aa62e
MarkAtEureka <44786078+MarkAtEureka@users.noreply.github.com>
2018-11-15 20:21:31 -0700

    testing selector

--- TextMagicTest.js
+++ TextMagicTest.js
@@ -1,3 +1,8 @@
-/* TextMagicTest
-*
+/*  TextMagicTest
+*   Can we concatenate two fields on a 123FormBuilder form?
 */
+var mobileNumber = '';
+var smsDomain = '';
+
+mobileNumber =  $('#form > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div > input[type="text"]').vlaue;
+alert(mobileNumber);
